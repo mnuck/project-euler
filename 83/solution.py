a96d4579077a421711cde3e38c69adcd255572df
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2012-05-06 23:12:55 -0500

    cleaned up 83

--- 83/solution.py
+++ 83/solution.py
@@ -2,8 +2,7 @@
 #
 # Find the cost of the lowest cost path from upper left to lower right
 
-from heapq import heappush as push
-from heapq import heappop as pop
+from heapq import heappush, heappop
 
 def main():
     with open('matrix.txt', 'r') as f:
@@ -18,16 +17,16 @@ def main():
 
     weights[(0,0)] = matrix[0][0]
     frontier = list()
-    push(frontier, (weights[(0,0)], (0,0)))
+    heappush(frontier, (weights[(0,0)], (0,0)))
          
     while len(frontier) > 0:
-        _, (x,y) = pop(frontier)
+        _, (x,y) = heappop(frontier)
         if (x,y) == (length-1, length-1):
             break
         for (i,j) in [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]:
             if (i,j) in weights and weights[(i,j)] == -1:
                 weights[(i,j)] = weights[(x,y)] + matrix[i][j]
-                push(frontier, (weights[(i,j)], (i,j)))
+                heappush(frontier, (weights[(i,j)], (i,j)))
     
     print weights[(length-1, length-1)]
     
