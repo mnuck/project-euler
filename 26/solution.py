763f1febec9114e281f7ad60296159eee7eaa918
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2012-05-07 22:57:55 -0500

    26 complete but ugly

--- 26/solution.py
+++ 26/solution.py
@@ -3,19 +3,23 @@
 # Find the value of d < 1000 for which 1/d contains the longest recurring 
 # cycle in its decimal fraction part.
 
+# not proud of this one, but it works
+
 def findRepeating(n):
     searchString = str( (10 **(2*n - 1) / n ))
-    result = 0
-    for x in xrange(1,n):
-        if( searchString[:2*x].count(searchString[:x-1]) == 2 ):
-            return x + 1
+    result = n - 1
+    for x in xrange(1,n+2):
+        substr  = searchString[-x:]
+        substr2 = searchString[-2*x:-x]
+        if substr == substr2:
+            return x
     return result
 
+
 maxRepeating = 0
-for n in xrange(1000,1,-1):
+for n in xrange(999,1,-1):
   repeating = findRepeating(n)
   if repeating > maxRepeating:
       maxRepeating = repeating
       print n, "repeats after ", repeating, "digits"
-      print str( (10 ** (2*n - 1) / n ))
       
