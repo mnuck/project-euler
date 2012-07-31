b843a3bce40e39130c30337532ccd67c955c3b7e
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2012-07-31 14:30:34 -0400

    46 solved

--- 46/solution.py
+++ 46/solution.py
@@ -2,33 +2,32 @@
 
 from math import sqrt
 
-primes = [2,3]
-squares = [1,4]
-
+primes = set([2,3])
 def isPrime(n):
-  trial = 2
-  primality = True
-  while( trial <= sqrt(n) ):
-    if( n % trial == 0 ):
-      primality = False
-      break
-    else:
-      trial += 1
-  if primality:
-    primes.append(n)
-  return primality
+  for prime in (x for x in primes if x <= sqrt(n)):
+    if n % prime == 0:
+      return False
+  primes.add(n)
+  return True
 
+def isTwiceSquare(n):
+  """Two is a non-odd prime, if this blows up, that's why"""
+  n /= 2.0
+  sqrt_n = int(sqrt(n))
+  return sqrt_n * sqrt_n == n
 
 def solution():
-  n = 7
+  n = 3
   while True:
     n += 2
     if not isPrime(n):
+      winner = True
       for prime in primes:
-        m = n - prime
-        if isSquare(m):
+        if isTwiceSquare(n - prime):
+          winner = False
           break
-      return n
+      if winner:
+        return n
 
 if __name__ == "__main__":
   print solution()
