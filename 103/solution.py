e1b0b087b4acdff2427eca7a51c3ff1a6a125602
= <matthew.nuckolls@gmail.com>
2021-11-20 19:07:16 -0800

    Add set_string()
    The problem statement asks us to format our solution as
    a string consisting of our set, sorted, each element
    converted to a string, and concatenated.

--- 103/solution.py
+++ 103/solution.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+def set_string(s):
+    result = ""
+    for item in sorted(s):
+        result += str(item)  # string concatenate
+    return result
 
 def solution():
-    return 12345
+    return set_string([1,2,3,4,5])
 
 if __name__ == "__main__":
     print(solution())
